b4bbf7e6f396c194868bb179f8261ab0e4e4d196
Kamil <kp054467@student.ath.edu.pl>
2020-03-04 07:08:06 +0100

    New files

---
+++ Exercise1/ZAD1/DB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ZAD1
+{
+    public class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+            var query = "SELECT * FROM Customers";
+            var command = new SqlCommand(query, connection);
+
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CompanyName"]);
+            }
+
+        }
+        public void Insert(SqlConnection connection, int id, string description)
+        {
+            var query = "INSERT INTO region(regionId, regionDescription)" +
+                "VALUES (@id, @description)";
+            var command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@description", description );
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+        }
+        public void Delete(SqlConnection connection, int id, string description)
+        {
+            var query = "DELETE FROM @region(regionId, regionDescription)" +
+                "VALUES (@id, @description)";
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("id",id);
+            command.Parameters.AddWithValue("description",description);
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"removed {affected}");
+            
+        }
+        public void Update(SqlConnection connection, int it, string description)
+        {
+            var query = " UPDATE region(regionId, regionDescription)" +
+                "VALUES (@id, @description);";
+            var command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@description", description);
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows update");
+        }
+    }
+}
---
+++ Exercise1/ZAD1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ZAD1
+{
+    class Program
+    {
+        //Zadanie z ADONET
+
+        static void Main(string[] args)
+        {
+            var db = new DB();
+            var connectionString = "";
+            using var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            db.Select(connection);
+            db.Insert(connection, 10,"Bielsko");
+            db.Update(connection, 10, "Bielsko-Biała");
+            db.Delete(connection, 10, "Bielsko");
+            
+            connection.Close();
+        }
+    }
+}
